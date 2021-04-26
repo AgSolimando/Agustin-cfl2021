b90d5a6311a86e9e95e98585d1177a5dc955ed8e
AgSolimando <82849594+AgSolimando@users.noreply.github.com>
2021-04-26 16:18:24 -0300

    clase 6

--- modulo 1/clase 6/eureka.js
+++ modulo 1/clase 6/eureka.js
@@ -1,8 +1,14 @@
-let readlinesync=require("readline-sync");
-let claveCorrecta = readlinesync.question("Inserte la clave alfabetica");
-let clave = "eureka"
-console.log(" Pedir clave correcta");
-let claveCorrecta=readlinesync.question("contraseña es");
-while (clavecorrecta == "eureka");
-Let contraseñaCorrecta= readlinesync.question("eureka")
-console.log("contraseña correcta es eureka");
+let readlineSync = require('readline-sync');
+let ingresoClave = readlineSync.question('inserte la clave correcta');
+let clave = "Eureka";
+let intento = 1;
+while ((ingresoClave != clave) && (intento < 3)){
+    intento++; 
+    ingresoClave = readlineSync.question('inserte la clave correcta'+" oportunidad"+intento); 
+}
+if (ingresoClave == clave){
+    console.log("ha ingresado con exito")
+    
+} else {
+    console.log("ha agotado las opciones")
+}
